6cc8003c09dd7845fce14df1d0fdb35b788646e7
Nguyễn Lê Thành Dũng <nguyentito@gmail.com>
2014-04-04 20:57:18 +0200

    Add signatures

--- trivial.hs
+++ trivial.hs
@@ -4,12 +4,15 @@ data Instr = PaintSQ Int Int Int
            | EraseCell Int Int
 type Solution = [Instr]
 
+printInstr :: Instr -> String
 printInstr (PaintSQ x y z) = "PAINTSQ " ++ (unwords . map show $ [x, y, z])
 printInstr (EraseCell x y) = "ERASECELL " ++ (unwords . map show $ [x, y])
 
+printSolution :: Solution -> IO ()
 printSolution sol =
   putStrLn $ show (length sol) ++ "\n" ++ unlines (map printInstr sol)
 
+parse :: String -> (Int, Int, U.Vector Bool)
 parse file =
   let (header:l) = lines file
       [strh, strw] = words header
@@ -20,9 +23,10 @@ parse file =
       t = U.fromList . map (== '#') . concat $ l
   in (h, w, t)
    
-      
+main :: IO ()      
 main = printSolution . solve . parse =<< readFile "doodle.txt"
 
+solve :: (Int, Int, U.Vector Bool) -> Solution
 solve (h, w, t) = undefined
   where t ! (i, j) = t U.! (i*h+j)
 
