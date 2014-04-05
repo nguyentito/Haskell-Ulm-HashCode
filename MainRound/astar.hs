364e8635d9ab55fd0a5fb74e433326fd37715e0c
Jonathan Laurent <jonathan-laurent@live.fr>
2014-04-05 14:09:54 +0200

    astar corrected

--- MainRound/astar.hs
+++ MainRound/astar.hs
@@ -1,25 +1,35 @@
 module Astar where
 
+import Data.List as L
+import Data.Maybe
+import Data.Set
+import qualified Data.Vector as V
+import Data.Functor
+
 import Data.Graph.AStar
 
+import Common
 
 mkIndexToSet g =
-  let indexToSet i = fromList (fst <$> (g ! i)) in
+  let indexToSet i = fromList (fst <$> (g V.! i)) in
   indexToSet
 
+mkDist :: Graph -> (Int -> Int -> Int)
 mkDist g =
   let dist i j =
-        let adjL = g ! i in
+        let adjL = g V.! i in
         let (_, (c, _)) = fromJust $ find (\ (k, _) -> k == j) adjL in c
                                                                        in dist
-
+sq :: Double -> Double
 sq x = x * x
 
 mkHeuristic gNodes iGoal =
-  let (goalX, goalY) = gNodes ! iGoal
+  let (goalX, goalY) = gNodes V.! iGoal
       heuristic i =
-        let (iX, iY) = gNodes ! i
-            dist = sqrt (sq (ix - goalX) + sq (iY - goalY)) in dist
+        let (iX, iY) = gNodes V.! i
+            dist = sqrt (sq (iX - goalX) + sq (iY - goalY)) in floor dist
+
+      in heuristic
 
 findAStar g gNodes i goal =
   fromJust $ aStar (mkIndexToSet g) (mkDist g) (mkHeuristic gNodes goal) (== goal) i
@@ -27,5 +37,5 @@ findAStar g gNodes i goal =
 aStarSol :: Graph -> GraphNodes -> Int -> Int -> (Int, [Int])
 aStarSol g gNodes i goal =
   let path = findAStar g gNodes i goal
-      (last, cost) = foldl (\ (l, c) j -> (j, c + mkDist g l j) (i, 0) path
-  (foldl' (), i : path)
+      (_, cost) = L.foldl' (\ (l, c) j -> (j, c + mkDist g l j)) (i, 0) path
+  in (cost, i : path)
